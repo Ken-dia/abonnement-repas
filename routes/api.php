f1f0263eac91eefbf33f1593b10f20aebb0f71cd
Hamidou Dia <diahamidou10@gmail.com>
2025-05-05 12:29:47 +0000

    supprimer la route utilisateur non utilisée

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\API\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::get('orders', [OrderController::class, 'index']);
 Route::post('order', [OrderController::class, 'store']);
 Route::delete('orders/{order}', [OrderController::class, 'destroy']);
